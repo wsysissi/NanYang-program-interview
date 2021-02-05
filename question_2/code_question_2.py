bb9776dc266cd31300573cd98d5e6c6aeaea81aa
wsysissi <32826316+wsysissi@users.noreply.github.com>
2021-02-05 21:48:00 +0800

    Update code_question_2.py

--- question_2/code_question_2.py
+++ question_2/code_question_2.py
@@ -1,11 +1,17 @@
 #matrix_f = open("input_question_4","r")
+class Matrix_Site:
+	def __init__ (self,val,label=None):
+		self.val = val
+		self.label = label
 matrix_f = open("test.txt","r")
 lines = matrix_f.readlines()
 matrix = []
 for line in lines:
-    line = line.strip("\n")
-    nums = line.split("\t")
+	line = line.strip("\n")
+	nums = line.split("\t")
+	sites = []
 	for i in nums :
-	
-    matrix.append(nums)
-
+		matrix_site = Matrix_Site(int(i))
+		sites.append(matrix_site)
+	matrix.append(sites)
+print(matrix)
